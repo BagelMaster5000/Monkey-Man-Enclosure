1cdf1e19a2d99a2cb57898bc11ccac6506612e9c
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-03-16 22:13:06 -0500

    Game controller, monkey spawning, level properties scriptable object

---
+++ Monkey Man Enclosure/Assets/Scripts/LevelSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelProperties", menuName = "ScriptableObjects/LevelProperties", order = 1)]
+public class LevelSO : ScriptableObject
+{
+    [Range(0, 5)]
+    public int monkeyAmount = 2;
+    [Range(0, 1)]
+    public float disruptionFrequency = 0.5f;
+}
