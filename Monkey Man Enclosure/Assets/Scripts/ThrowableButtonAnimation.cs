cce50d731ec81fcb2bf617456d95778a824fd6cd
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-03-20 09:53:57 -0500

    Imported UI Graphics

---
+++ Monkey Man Enclosure/Assets/Scripts/ThrowableButtonAnimation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowableButtonAnimation : MonoBehaviour
+{
+    [SerializeField] Transform objectGraphic;
+    Vector2 objectGraphicStartingPosition;
+
+    float shakeAmt = 0;
+
+    Toggle toggleComponent;
+    bool selected;
+
+    private void Awake()
+    {
+        objectGraphicStartingPosition = objectGraphic.localPosition;
+        toggleComponent = GetComponent<Toggle>();
+    }
+
+    void Update()
+    {
+        selected = toggleComponent.isOn;
+
+        if (shakeAmt > 0)
+        {
+            objectGraphic.localPosition = objectGraphicStartingPosition +
+                new Vector2(
+                    Random.Range(-shakeAmt, shakeAmt),
+                    Random.Range(-shakeAmt, shakeAmt));
+        }
+        else
+            objectGraphic.localPosition = objectGraphicStartingPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        if (shakeAmt > 0)
+            shakeAmt -= 0.35f;
+    }
+
+    public void Shake(float setShakeAmt) => shakeAmt = setShakeAmt;
+}
