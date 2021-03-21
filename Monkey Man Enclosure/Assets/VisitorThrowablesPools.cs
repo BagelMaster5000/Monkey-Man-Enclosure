7b10944b526177fccfa2d613fc54a347be953c4b
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-03-21 16:10:30 -0500

    Visitors now throw

---
+++ Monkey Man Enclosure/Assets/VisitorThrowablesPools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class VisitorThrowablesPools : MonoBehaviour
+{
+    public static VisitorThrowablesPools instance;
+    public GameObject[] foodPelletsPool;
+    public GameObject[] bricksPool;
+    public GameObject[] bananasPool;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
