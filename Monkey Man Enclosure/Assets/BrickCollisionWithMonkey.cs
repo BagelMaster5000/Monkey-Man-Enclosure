1f45f6c626624f54abff46887ef01fd464185d95
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-03-22 20:00:14 -0500

    Projectile poo set to occur when monkey hit with brick

---
+++ Monkey Man Enclosure/Assets/BrickCollisionWithMonkey.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BrickCollisionWithMonkey : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Monkey"))
+            collision.gameObject.GetComponent<Monkey>().ProjectilePoop();
+
+        Destroy(this);
+    }
+}
